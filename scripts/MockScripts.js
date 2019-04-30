61bdb28acbf89d0f3c884a1e4aa42a98466df686
mdcoon <mdcoon1@yahoo.com>
2019-04-30 06:43:54 -0400

    Fixed mock script to initialize and call request data instead of addTip

--- scripts/MockScripts.js
+++ scripts/MockScripts.js
@@ -11,7 +11,7 @@ Run mockMiner.py
 */
 const Web3 = require('web3')
 const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'));
-const BN = require('bn.js');  
+const BN = require('bn.js');
 const TellorMaster = artifacts.require("./TellorMaster.sol");
 const Oracle = artifacts.require("./Tellor.sol");
 var oracleAbi = Oracle.abi;
@@ -34,9 +34,8 @@ module.exports = async function(callback) {
     oracle = await TellorMaster.deployed()
     console.log('oracle address',oracle.address)
     oracle2 = await new web3.eth.Contract(oracleAbi,oracle.address);///will this instance work for logWatch? hopefully...
-    //web3.eth.sendTransaction({to: oracle.address,from:acct,gas:7000000, data: web3.utils.keccak256("tellorPostConstructor()")})
-    //web3.eth.sendTransaction({to: oracle.address,from:acct,gas:7000000,data:oracle2.methods.requestData(api,"BTC/USD",0,1000,0).encodeABI()});
-    web3.eth.sendTransaction({to: oracle.address,from:acct,gas:7000000,data:oracle2.methods.addTip(1,1).encodeABI()});
+    web3.eth.sendTransaction({to: oracle.address,from:acct,gas:7000000, data: web3.utils.keccak256("tellorPostConstructor()")})
+    web3.eth.sendTransaction({to: oracle.address,from:acct,gas:7000000,data:oracle2.methods.requestData(api,"BTC/USD",0,1000,0).encodeABI()});
     console.log('Ready!')
     return true;
 }
