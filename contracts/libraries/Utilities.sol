baf26e91d2cb8a0b982c8443a711d98baf8e51fe
themandalore <fett.nicholas@yahoo.com>
2019-06-28 15:52:49 -0400

    nickauditupdates

--- contracts/libraries/Utilities.sol
+++ contracts/libraries/Utilities.sol
@@ -6,49 +6,32 @@ pragma solidity ^0.5.0;
 library Utilities{
 
   /// @dev Returns the minimum value and position in an array.
-    // function getMin(uint256[51] memory arr) internal pure returns (uint256 min, uint256 minIndex) {
-    //   assembly {
-    //       let arrayLen := mload(arr)
-    //       let arrayStart := add(arr, 0x20)
-    //       minIndex := sub(arrayLen,1)
-    //       min := mload(add(arrayStart, mul(minIndex , 0x20)))
-    //       for {let i := minIndex } gt(i,0) { i := sub(i, 1) } {
-    //           let item := mload(add(arrayStart, mul(sub(i,1), 0x20)))
-    //           if lt(item,min){
-    //               min := item
-    //               minIndex := sub(i,1)
-    //           }
-    //       }
-    //   }
-    // }
+  //@note IT IGNORES THE 0 INDEX
+    function getMin(uint[51] memory arr) internal pure returns (uint256 min, uint256 minIndex) {
+      assembly {
+          minIndex := 50
+          min := mload(add(arr, mul(minIndex , 0x20)))
+          for {let i := 49 } gt(i,0) { i := sub(i, 1) } {
+              let item := mload(add(arr, mul(i, 0x20)))
+              if lt(item,min){
+                  min := item
+                  minIndex := i
+              }
+          }
+      }
+    }
 
-  function getMin(uint[51] memory data) internal pure returns(uint256 minimal,uint minIndex) {
-        minIndex = data.length - 1;
-        minimal = data[minIndex];
-        for(uint i = data.length-1;i > 0;i--) {
-            if(data[i] < minimal) {
-                minimal = data[i];
-                minIndex = i;
-            }
-        }
-  }
 
   function getMax(uint256[51] memory arr) internal pure returns (uint256 max, uint256 maxIndex) {
       assembly {
-          let arrayLen := mload(arr)
-          let arrayStart := add(arr, 0x20)
-
-          for { let i := 0 } lt(i, arrayLen) { i := add(i, 1) } {
-              let item := mload(add(arrayStart, mul(i, 0x20)))
-
+          for { let i := 0 } lt(i,51) { i := add(i, 1) } {
+              let item := mload(add(arr, mul(i, 0x20)))
               if lt(max, item) {
                   max := item
                   maxIndex := i
               }
           }
       }
-
-      return (max, maxIndex);
     }
 
   }
