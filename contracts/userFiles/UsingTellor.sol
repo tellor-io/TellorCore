069d7b41732a75b9b58e27c614cb2fbb978794b2
Brenda <l.loya.b@gmail.com>
2019-07-01 19:38:41 -0400

    comments to userContract and usingTellor

--- contracts/userFiles/UsingTellor.sol
+++ contracts/userFiles/UsingTellor.sol
@@ -1,107 +1,176 @@
-// pragma solidity ^0.5.0;
+pragma solidity ^0.5.0;
 
-// import '../Tellor.sol';
-// import '../TellorMaster.sol';
-// import './UserContract.sol';
-// /**
-// * @title UsingTellor
-// * This contracts creates for easy integration to the Tellor Tellor System
-// */
-// contract UsingTellor{
-// 	UserContract tellorUserContract;
-// 	address public owner;
+import '../Tellor.sol';
+import '../TellorMaster.sol';
+import './UserContract.sol';
+/**
+* @title UsingTellor
+* This contracts creates for easy integration to the Tellor Tellor System
+*/
+contract UsingTellor{
+	UserContract tellorUserContract;
+	address public owner;
 	
-// 	event OwnershipTransferred(address _previousOwner,address _newOwner);
+	event OwnershipTransferred(address _previousOwner,address _newOwner);
+
+    /*Constructor*/
+    /**
+    * @dev This function sents the owner and userContract address
+    * @param _user is the UserContract.sol address
+    */
+    constructor(address _user)public {
+    	tellorUserContract = UserContract(_user);
+    	owner = msg.sender;
+    }
 
 
-//     constructor(address _user)public {
-//     	tellorUserContract = UserContract(_user);
-//     	owner = msg.sender;
-//     }
+    /*Functions*/
+    /**
+    * @dev Allows the user to get the latest value for the requestId specified
+    * @param _requestId is the requestId to look up the value for
+    * @return bool true if it is able to retreive a value, the value, and the value's timestamp
+    */
+	function getCurrentValue(uint _requestId) public view returns(bool ifRetrieve, uint value, uint _timestampRetrieved) {
+		TellorMaster _tellor = TellorMaster(tellorUserContract.tellorStorageAddress());
+		uint _count = _tellor.getNewValueCountbyRequestId(_requestId) ;
+		if(_count > 0){
+				_timestampRetrieved = _tellor.getTimestampbyRequestIDandIndex(_requestId,_count -1);//will this work with a zero index? (or insta hit?)
+				return(true,_tellor.retrieveData(_requestId,_timestampRetrieved),_timestampRetrieved);
+        }
+        return(false,0,0);
+	}
 
-// 	function getCurrentValue(uint _requestId) public view returns(bool ifRetrieve, uint value, uint _timestampRetrieved) {
-// 		TellorMaster _tellor = TellorMaster(tellorUserContract.tellorStorageAddress());
-// 		uint _count = _tellor.getNewValueCountbyRequestId(_requestId) ;
-// 		if(_count > 0){
-// 				_timestampRetrieved = _tellor.getTimestampbyRequestIDandIndex(_requestId,_count -1);//will this work with a zero index? (or insta hit?)
-// 				return(true,_tellor.retrieveData(_requestId,_timestampRetrieved),_timestampRetrieved);
-//         }
-//         return(false,0,0);
-// 	}
 
-// 	//How can we make this one more efficient?
-// 	function getFirstVerifiedDataAfter(uint _requestId, uint _timestamp) public returns(bool,uint,uint _timestampRetrieved) {
-// 		TellorMaster _tellor = TellorMaster(tellorUserContract.tellorStorageAddress());
-// 		uint _count = _tellor.getNewValueCountbyRequestId(_requestId);
-// 		if(_count > 0){
-// 				for(uint i = _count;i > 0;i--){
-// 					if(_tellor.getTimestampbyRequestIDandIndex(_requestId,i-1) > _timestamp && _tellor.getTimestampbyRequestIDandIndex(_requestId,i-1) < block.timestamp - 86400){
-// 						_timestampRetrieved = _tellor.getTimestampbyRequestIDandIndex(_requestId,i-1);//will this work with a zero index? (or insta hit?)
-// 					}
-// 				}
-// 				if(_timestampRetrieved > 0){
-// 					return(true,_tellor.retrieveData(_requestId,_timestampRetrieved),_timestampRetrieved);
-// 				}
-//         }
-//         return(false,0,0);
-// 	}
+	//How can we make this one more efficient?
+	/**
+    * @dev Allows the user to get the first verified value for the requestId after the specified timestamp
+    * @param _requestId is the requestId to look up the value for
+    * @param _timestamp after which to search for first verified value
+    * @return bool true if it is able to retreive a value, the value, and the value's timestamp, the timestamp after
+    * which it searched for the first verified value
+    */
+	function getFirstVerifiedDataAfter(uint _requestId, uint _timestamp) public returns(bool,uint,uint _timestampRetrieved) {
+		TellorMaster _tellor = TellorMaster(tellorUserContract.tellorStorageAddress());
+		uint _count = _tellor.getNewValueCountbyRequestId(_requestId);
+		if(_count > 0){
+				for(uint i = _count;i > 0;i--){
+					if(_tellor.getTimestampbyRequestIDandIndex(_requestId,i-1) > _timestamp && _tellor.getTimestampbyRequestIDandIndex(_requestId,i-1) < block.timestamp - 86400){
+						_timestampRetrieved = _tellor.getTimestampbyRequestIDandIndex(_requestId,i-1);//will this work with a zero index? (or insta hit?)
+					}
+				}
+				if(_timestampRetrieved > 0){
+					return(true,_tellor.retrieveData(_requestId,_timestampRetrieved),_timestampRetrieved);
+				}
+        }
+        return(false,0,0);
+	}
 	
-// 	event Print(string _s,uint _num);
+	event Print(string _s,uint _num);
 
-// 	function getAnyDataAfter(uint _requestId, uint _timestamp) public  returns(bool _ifRetrieve, uint _value, uint _timestampRetrieved){
-// 		TellorMaster _tellor = TellorMaster(tellorUserContract.tellorStorageAddress());
-// 		uint _count = _tellor.getNewValueCountbyRequestId(_requestId) ;
-// 		if(_count > 0){
-// 				emit Print("count",_count);
-// 				for(uint i = _count;i > 0;i--){
-// 					emit Print('tester',_tellor.getTimestampbyRequestIDandIndex(_requestId,i-1));
-// 					emit Print('actual', _timestamp);
 
-// 					if(_tellor.getTimestampbyRequestIDandIndex(_requestId,i-1) >= _timestamp){
-// 						_timestampRetrieved = _tellor.getTimestampbyRequestIDandIndex(_requestId,i-1);//will this work with a zero index? (or insta hit?)
-// 						emit Print("_timestampRetrieved",_timestampRetrieved);
-// 						emit Print("value",_tellor.retrieveData(_requestId,_timestampRetrieved));
-// 					}
-// 				}
-// 				if(_timestampRetrieved > 0){
-// 					return(true,_tellor.retrieveData(_requestId,_timestampRetrieved),_timestampRetrieved);
-// 				}
-//         }
-//         return(false,0,0);
-// 	}
+	/**
+    * @dev Allows the user to get the first value for the requestId after the specified timestamp
+    * @param _requestId is the requestId to look up the value for
+    * @param _timestamp after which to search for first verified value
+    * @return bool true if it is able to retreive a value, the value, and the value's timestamp
+    */
+	function getAnyDataAfter(uint _requestId, uint _timestamp) public  returns(bool _ifRetrieve, uint _value, uint _timestampRetrieved){
+		TellorMaster _tellor = TellorMaster(tellorUserContract.tellorStorageAddress());
+		uint _count = _tellor.getNewValueCountbyRequestId(_requestId) ;
+		if(_count > 0){
+				emit Print("count",_count);
+				for(uint i = _count;i > 0;i--){
+					emit Print('tester',_tellor.getTimestampbyRequestIDandIndex(_requestId,i-1));
+					emit Print('actual', _timestamp);
 
-// 	function requestData(string calldata _request,string calldata _symbol,uint _granularity, uint _tip) external{
-// 		Tellor _tellor = Tellor(tellorUserContract.tellorStorageAddress());
-// 		if(_tip > 0){
-// 			require(_tellor.transferFrom(msg.sender,address(this),_tip));
-// 		}
-// 		_tellor.requestData(_request,_symbol,_granularity,_tip);
-// 	}
+					if(_tellor.getTimestampbyRequestIDandIndex(_requestId,i-1) >= _timestamp){
+						_timestampRetrieved = _tellor.getTimestampbyRequestIDandIndex(_requestId,i-1);//will this work with a zero index? (or insta hit?)
+						emit Print("_timestampRetrieved",_timestampRetrieved);
+						emit Print("value",_tellor.retrieveData(_requestId,_timestampRetrieved));
+					}
+				}
+				if(_timestampRetrieved > 0){
+					return(true,_tellor.retrieveData(_requestId,_timestampRetrieved),_timestampRetrieved);
+				}
+        }
+        return(false,0,0);
+	}
 
-// 	function requestDataWithEther(string calldata _request,string calldata _symbol,uint _granularity, uint _tip) payable external{
-// 		tellorUserContract.requestDataWithEther.value(msg.value)(_request,_symbol,_granularity,_tip);
-// 	}
 
-// 	function addTip(uint _requestId, uint _tip) public {
-// 		Tellor _tellor = Tellor(tellorUserContract.tellorStorageAddress());
-// 		require(_tellor.transferFrom(msg.sender,address(this),_tip));
-// 		_tellor.addTip(_requestId,_tip);
-// 	}
+    /**
+    * @dev Allows the user to submit a request for data to the oracle using Tributes
+    * Allowing this prevents us from increasing spread too high (since if we set the price too hight
+	* users will just go to an exchange and get tokens from there)
+    * @param _request string API being requested to be mined
+    * @param _symbol is the short string symbol for the api request
+    * @param _granularity is the number of decimals miners should include on the submitted value
+    * @param _tip amount the requester is willing to pay to be get on queue. Miners
+    * mine the onDeckQueryHash, or the api with the highest payout pool
+    */
+	function requestData(string calldata _request,string calldata _symbol,uint _granularity, uint _tip) external{
+		Tellor _tellor = Tellor(tellorUserContract.tellorStorageAddress());
+		if(_tip > 0){
+			require(_tellor.transferFrom(msg.sender,address(this),_tip));
+		}
+		_tellor.requestData(_request,_symbol,_granularity,_tip);
+	}
 
-// 	function addTipWithEther(uint _requestId, uint _tip) public payable {
-// 		UserContract(tellorUserContract).addTipWithEther.value(msg.value)(_requestId,_tip);
-// 	}
 
-// 	function setUserContract(address _userContract) public {
-// 		require(msg.sender == owner);//who should this be?
-// 		tellorUserContract = UserContract(_userContract);
-// 	}
+    /**
+    * @dev Allows the user to submit a request for data to the oracle using ETH
+    * @param _request string API being requested to be mined
+    * @param _symbol is the short string symbol for the api request
+    * @param _granularity is the number of decimals miners should include on the submitted value
+    * @param _tip amount the requester is willing to pay to be get on queue. Miners
+    * mine the onDeckQueryHash, or the api with the highest payout pool
+    */
+	function requestDataWithEther(string calldata _request,string calldata _symbol,uint _granularity, uint _tip) payable external{
+		tellorUserContract.requestDataWithEther.value(msg.value)(_request,_symbol,_granularity,_tip);
+	}
 
-// 	function transferOwnership(address payable _newOwner) external {
-//             require(msg.sender == owner);
-//             emit OwnershipTransferred(owner, _newOwner);
-//             owner = _newOwner;
-//     }
-// }
+
+    /** 
+    * @dev Allows the user to tip miners for the specified request using Tributes
+    * @param _requestId to tip
+    * @param _tip amount
+    */
+	function addTip(uint _requestId, uint _tip) public {
+		Tellor _tellor = Tellor(tellorUserContract.tellorStorageAddress());
+		require(_tellor.transferFrom(msg.sender,address(this),_tip));
+		_tellor.addTip(_requestId,_tip);
+	}
+
+
+    /**
+    * @dev Allows user to add tip with Ether by sending the ETH to the userContract and exchanging it for Tributes
+    * at the price specified by the userContract owner.
+    * @param _requestId to tip
+    * @param _tip amount
+    */
+	function addTipWithEther(uint _requestId, uint _tip) public payable {
+		UserContract(tellorUserContract).addTipWithEther.value(msg.value)(_requestId,_tip);
+	}
+
+
+    /**
+    * @dev allows owner to set the user contract address
+    * @param _userContract address
+    */
+	function setUserContract(address _userContract) public {
+		require(msg.sender == owner);//who should this be?
+		tellorUserContract = UserContract(_userContract);
+	}
+
+
+    /**
+    * @dev allows owner to transfer ownership
+    * @param _newOwner address
+    */
+	function transferOwnership(address payable _newOwner) external {
+            require(msg.sender == owner);
+            emit OwnershipTransferred(owner, _newOwner);
+            owner = _newOwner;
+    }
+}
 
 
