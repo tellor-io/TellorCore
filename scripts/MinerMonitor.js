b0379aa19d15bb1b9717d40600f1897ca9383218
brendaloya <l.loya.b@gmail.com>
2019-08-29 16:52:10 +0000

    need to work on readfile

--- scripts/MinerMonitor.js
+++ scripts/MinerMonitor.js
@@ -1,6 +1,7 @@
 //require('dotenv').config();
 const Web3 = require('web3');
 var HDWalletProvider = require("truffle-hdwallet-provider");
+var fs = require('fs');
 
 /**
 *Send Oraclize query for the Eth oracle
@@ -31,7 +32,7 @@ module.exports =async function(callback) {
 let bal = await web3.eth.getBalance(i);
 console.log(i, web3.utils.fromWei(bal));*/
     let miners = ['0xb9dD5AfD86547Df817DA2d0Fb89334A6F8eDd891',
-    '0x230570cd052f40e14c14a81038c6f3aa685d712b']
+    '0x230570cD052f40E14C14a81038c6f3aa685d712B'];
 /*
     let mainMiners=['0xC840ba62Aab90B8cD629649822F04300Ef5D1963',
     '0x3564E17D5f6B7c9A3c6Bd6248BF7B3EeB4927e50',
@@ -44,13 +45,47 @@ console.log(i, web3.utils.fromWei(bal));*/
     ];
 */
  let i;
-    for(i = 0; i <= 2; i++){
+    for(i = 0; i <= 1; i++){
         try{
             let minerAddress = miners[i];
             let bal = await web3.eth.getBalance(minerAddress);
-            console.log(i, web3.utils.fromWei(bal));
+            console.log(i,minerAddress, web3.utils.fromWei(bal));
+            let txnonce = await web3.eth.getTransactionCount(minerAddress);
+            let pendnonce = await web3.eth.getTransactionCount(minerAddress, 'pending');
+            console.log("tx", i,minerAddress,txnonce );
+            console.log("tx pending",i,minerAddress,pendnonce );
+            var name = "miner" + i;
+            name = {miner: i,
+                    txnonce: txnonce}
+            var jsonName = JSON.stringify(name);
+            console.log("jsonName", jsonName);
+            let filename = "./scripts/miner" + i + ".json";
+            fs.writeFile(filename, jsonName, function(err) {
+                if (err) {
+                    console.log(err);
+                }
+            });
         } catch(error) {
         console.error(error);
         }
     }
+
+    for(let b = 0; b <= 1; b++){
+        try{
+            let minerAddress = miners[b];
+            let txnonceNow = await web3.eth.getTransactionCount(minerAddress);
+            let filename = "./scripts/miner" + b + ".json";
+            fs.readFile(filename,'utf8', jsonName, function(err, data) {
+                if (err) throw err;
+                let minerInfo = JSON.parse(data);
+                console.log("minerInfo", minerInfo);
+            });
+        
+        } catch(error) {
+        console.error(error);
+        }
+    }
+    
+
+
 }
