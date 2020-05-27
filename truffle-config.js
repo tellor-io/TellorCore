58c366993f9b6084c3ad8e0f5a6c80704d6b87db
Brenda <l.loya.b@gmail.com>
2020-05-27 16:27:10 -0400

    monitors

--- truffle-config.js
+++ truffle-config.js
@@ -22,7 +22,7 @@ module.exports = {
     },
     mainnet: {
       provider: () =>
-        new HDWalletProvider("77a597bedae2ffcd93dac5a721f74e0119ce6719d4ccd0de340153c734255ab1", `https://mainnet.infura.io/v3/${accessToken}`),
+        new HDWalletProvider("", `https://mainnet.infura.io/v3/${accessToken}`),
       network_id: 1,
       gas: 200000,
       gasPrice: 8000000000,
