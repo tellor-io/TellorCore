6562e507a9954716943c51bb00c31ae1fa685e5b
JG Carvalho <jg@jgcarv.com>
2020-10-07 11:18:39 -0300

    added more tests

--- test/v25.js
+++ test/v25.js
@@ -1,9 +1,86 @@
+const helper = require("./helpers/test_helpers");
 const TestLib = require("./helpers/testLib");
+const Tellor = artifacts.require("./TellorTest.sol"); // globally injected artifacts helper
+const TransitionContract = artifacts.require("./TellorTransition");
 
-contract("V2.5 Tests", function(accounts) {
+const hash = web3.utils.keccak256;
+const BN = web3.utils.BN;
+
+contract("Tests for V2.5", function(accounts) {
+  let master;
+  let env;
+  //Hardcoded adress because they need to be known when the TransitionCOntract is compiled
+  const baseAdd = "0x6511D2957aa09350494f892Ce2881851f0bb26D3";
+  const newAdd = "0x032Aa32e4069318b15e6462CE20926d4d821De90";
+  const oldStake = new BN(web3.utils.toWei("1000", "ether"));
+  const newStake = new BN(web3.utils.toWei("500", "ether"));
+
+  const upgrade = async () => {
+    let newTellor = await Tellor.new({ from: accounts[9] });
+    transitionContract = await TransitionContract.new();
+    newTellor = await Tellor.at(newAdd);
+
+    vars = await master.getNewCurrentVariables();
+    await master.changeTellorContract(transitionContract.address);
+
+    await helper.advanceTime(60 * 16);
+    await TestLib.mineBlock(env, accounts);
+  };
+
   beforeEach("Setup contract for each test", async function() {
-    let lib = await TestLib.getV2(accounts);
+    master = await TestLib.getV2(accounts, true);
+    env = {
+      master: master,
+      accounts: accounts,
+    };
   });
 
-  it("Test Proposing Transotion Contract", async function() {});
+  describe("Staking and Withdrawing", async () => {
+    it("Stake amount changes correctly", async () => {
+      await upgrade();
+      let stakeAmount = await master.getUintVar(hash("stakeAmount"));
+      assert(
+        stakeAmount.eq(newStake),
+        "contract should set stake amount transition properly"
+      );
+    });
+
+    it("New miner can stake new stake amount", async () => {
+      await upgrade();
+
+      let staker = accounts[8];
+      let info = await master.getStakerInfo(staker);
+
+      assert(info["0"].toString() == "0", "Account shouldn't be staked");
+      await master.approve(master.address, newStake, { from: staker });
+      await master.depositStake({ from: staker });
+
+      let info2 = await master.getStakerInfo(staker);
+
+      assert(info2["0"].toString() == "1", "Account should be staked");
+    });
+
+    it("Previous staker can withdraw difference", async () => {
+      let staker = accounts[8];
+      let previousBal = await master.balanceOf(staker);
+
+      await master.approve(master.address, newStake, { from: staker });
+      await master.depositStake({ from: staker });
+
+      let info = await master.getStakerInfo(staker);
+      assert(info["0"].toString() == "1", "Account should be staked");
+
+      await upgrade();
+
+      let info2 = await master.getStakerInfo(staker);
+      assert(info2["0"].toString() == "1", "Account should be staked");
+
+      await master.transfer(accounts[0], previousBal.sub(newStake), {
+        from: staker,
+      });
+
+      let newBalance = await master.balanceOf(staker);
+      assert(newBalance.eq(newStake), "Staker should have only the stake left");
+    });
+  });
 });
