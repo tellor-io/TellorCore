ec7550f3aaeff793e039b967429560fea94f3e1f
themandalore <fett.nicholas@yahoo.com>
2020-10-08 15:03:11 -0400

    furtherTests done

--- test/furtherTests.js
+++ test/furtherTests.js
@@ -1,128 +1,76 @@
-// const TellorMaster = artifacts.require("./TellorMaster.sol");
-// const Tellor = artifacts.require("./TellorTest.sol"); // globally injected artifacts helper
-
-// var oracleAbi = Tellor.abi;
-// var masterAbi = TellorMaster.abi;
-
-// contract("Further Tests", function(accounts) {
-//   let oracle;
-//   let oracle2;
-//   let oracleBase;
+// const TestLib = require("./helpers/testLib");
+// const helper = require("./helpers/test_helpers");
+// const Tellor = artifacts.require("./TellorTest.sol");
+// contract("Further tests", function(accounts) {
 //   let master;
-//   let utilities;
+//   let env;
 
 //   beforeEach("Setup contract for each test", async function() {
-//     oracleBase = await Tellor.new();
-//     oracle = await TellorMaster.new(oracleBase.address);
-//     master = await new web3.eth.Contract(masterAbi, oracle.address);
-//     oracle2 = await new web3.eth.Contract(oracleAbi, oracleBase.address);
+//     //Could use the getV25(accounts, true), since you're upgrading in the first line of tests. I added full tips to getV25 in testLib already
+//     master = await TestLib.getV25(accounts, true);
+//     env = {
+//       master: master,
+//       accounts: accounts,
+//     };
 //   });
 
 //   it("transferOwnership", async function() {
-//     let checkowner = await oracle.getAddressVars(
+//     let checkowner = await master.getAddressVars(
 //       web3.utils.keccak256("_owner")
 //     );
 //     assert(checkowner == accounts[0], "initial owner acct 0");
-//     await web3.eth.sendTransaction({
-//       to: oracle.address,
-//       from: accounts[0],
-//       gas: 7000000,
-//       data: oracle2.methods.proposeOwnership(accounts[2]).encodeABI(),
-//     });
-//     let pendingOwner = await oracle.getAddressVars(
+//     await master.proposeOwnership(accounts[2])
+//     let pendingOwner = await master.getAddressVars(
 //       web3.utils.keccak256("pending_owner")
 //     );
 //     assert(pendingOwner == accounts[2], "pending owner acct 2");
-//     checkowner = await oracle.getAddressVars(web3.utils.keccak256("_owner"));
+//     checkowner = await master.getAddressVars(web3.utils.keccak256("_owner"));
 //     assert(checkowner == accounts[0], "initial owner acct 0");
-//     await web3.eth.sendTransaction({
-//       to: oracle.address,
-//       from: accounts[2],
-//       gas: 7000000,
-//       data: oracle2.methods.claimOwnership().encodeABI(),
-//     });
-//     checkowner = await oracle.getAddressVars(web3.utils.keccak256("_owner"));
+//     await master.claimOwnership({from:accounts[2]})
+//     checkowner = await master.getAddressVars(web3.utils.keccak256("_owner"));
 //     assert(checkowner == accounts[2], "new owner acct 2");
 //   });
 //   it("Test Deity Functions", async function() {
-//     let owner = await oracle.getAddressVars(web3.utils.keccak256("_deity"));
+//     let owner = await master.getAddressVars(web3.utils.keccak256("_deity"));
 //     assert(owner == accounts[0]);
-//     await web3.eth.sendTransaction({
-//       to: oracle.address,
-//       from: accounts[0],
-//       gas: 7000000,
-//       data: master.methods.changeDeity(accounts[1]).encodeABI(),
-//     });
-//     owner = await oracle.getAddressVars(web3.utils.keccak256("_deity"));
+//     await master.changeDeity(accounts[1])
+//     owner = await master.getAddressVars(web3.utils.keccak256("_deity"));
 //     assert(owner == accounts[1]);
-//     newOracle = await Tellor.new();
-//     await web3.eth.sendTransaction({
-//       to: oracle.address,
-//       from: accounts[1],
-//       gas: 7000000,
-//       data: master.methods.changeTellorContract(newOracle.address).encodeABI(),
-//     });
+//     let newOracle = await Tellor.new();
+//     master.changeTellorContract(newOracle.address,{from:accounts[1]})
 //     assert(
-//       (await oracle.getAddressVars(web3.utils.keccak256("tellorContract"))) ==
+//       (await master.getAddressVars(web3.utils.keccak256("tellorContract"))) ==
 //         newOracle.address
 //     );
 //   });
 //   it("Get Symbol and decimals", async function() {
-//     let symbol = await oracle2.methods.symbol().call();
+//     let symbol = await master.symbol()
 //     assert.equal(symbol, "TRB", "the Symbol should be TT");
-//     data3 = await oracle2.methods.decimals().call();
+//     data3 = await master.decimals()
 //     assert(data3 - 0 == 18);
 //   });
 //   it("Get name", async function() {
-//     let name = await oracle2.methods.name().call();
+//     let name = await master.name();
 //     assert.equal(name, "Tellor Tributes", "the Name should be Tellor Tributes");
 //   });
 
 //   it("Total Supply", async function() {
-//     supply = await master.methods.totalSupply().call();
-//     assert.equal(web3.utils.fromWei(supply), 6000, "Supply should be 6000"); //added miner
+//     supply = await master.totalSupply();
+//     assert(web3.utils.fromWei(supply) > 0, "Supply should not be 0"); //added miner
+//     assert(web3.utils.fromWei(supply) < 100000, "Supply should be less than 100k"); //added miner
 //   });
 //   it("Test Changing Dispute Fee", async function() {
-//     await web3.eth.sendTransaction({
-//       to: oracle.address,
-//       from: accounts[0],
-//       gas: 7000000,
-//       data: oracle2.methods
-//         .theLazyCoon(accounts[6], web3.utils.toWei("5000", "ether"))
-//         .encodeABI(),
-//     });
-//     await web3.eth.sendTransaction({
-//       to: oracle.address,
-//       from: accounts[0],
-//       gas: 7000000,
-//       data: oracle2.methods
-//         .theLazyCoon(accounts[7], web3.utils.toWei("5000", "ether"))
-//         .encodeABI(),
-//     });
-//     var disputeFee1 = await oracle.getUintVar(
+//     await  master.theLazyCoon(accounts[6], web3.utils.toWei("5000", "ether"))
+//     await master.theLazyCoon(accounts[7], web3.utils.toWei("5000", "ether"))
+//     var disputeFee1 = await master.getUintVar(
 //       web3.utils.keccak256("disputeFee")
 //     );
 //     newOracle = await Tellor.new();
-//     await web3.eth.sendTransaction({
-//       to: oracle.address,
-//       from: accounts[0],
-//       gas: 7000000,
-//       data: master.methods.changeTellorContract(newOracle.address).encodeABI(),
-//     });
-//     await web3.eth.sendTransaction({
-//       to: oracle.address,
-//       from: accounts[6],
-//       gas: 7000000,
-//       data: oracle2.methods.depositStake().encodeABI(),
-//     });
-//     await web3.eth.sendTransaction({
-//       to: oracle.address,
-//       from: accounts[7],
-//       gas: 7000000,
-//       data: oracle2.methods.depositStake().encodeABI(),
-//     });
+//     await master.changeTellorContract(newOracle.address)
+//     await master.depositStake({from:accounts[6]})
+//     await  master.depositStake({from:accounts[7]})
 //     assert(
-//       (await oracle.getUintVar(web3.utils.keccak256("disputeFee"))) <
+//       (await master.getUintVar(web3.utils.keccak256("disputeFee"))) <
 //         disputeFee1,
 //       "disputeFee should change"
 //     );
