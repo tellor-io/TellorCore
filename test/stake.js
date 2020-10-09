b268283f19e75d7375042887277b901bfe2e86b3
JG Carvalho <jg@jgcarv.com>
2020-10-09 17:36:59 -0300

    request tests

--- test/stake.js
+++ test/stake.js
@@ -1,4 +1,3 @@
-
 const Tellor = artifacts.require("./TellorTest.sol"); // globally injected artifacts helper
 var masterAbi = Tellor.abi;
 const TestLib = require("./helpers/testLib");
@@ -38,7 +37,10 @@ contract("Staking Tests", function(accounts) {
   // });
 
   it("Staking, requestStakingWithdraw, withdraw stake", async function() {
-    let withdrawreq = await master.requestStakingWithdraw({from:accounts[1]})
+    let withdrawreq = await master.requestStakingWithdraw({
+      from: accounts[1],
+    });
+    console.log(withdrawreq.logs);
     let weSender = await web3.eth.abi.decodeParameter(
       "address",
       withdrawreq.logs[0].topics[1]
@@ -47,24 +49,28 @@ contract("Staking Tests", function(accounts) {
     await helper.advanceTime(86400 * 8);
     s = await master.getStakerInfo(accounts[1]);
     assert(s != 1, " Staked");
-    await master.withdrawStake({from:accounts[1]})
+    await master.withdrawStake({ from: accounts[1] });
     s = await master.getStakerInfo(accounts[1]);
     assert(s != 1, "not Staked");
   });
   it("Attempt to Allow and transferFrom more than balance - stake", async function() {
-    await master.theLazyCoon(accounts[2], web3.utils.toWei("5000", "ether"))
-    await master.transfer(accounts[8], web3.utils.toWei("500"),{from:accounts[1]})
+    await master.theLazyCoon(accounts[2], web3.utils.toWei("5000", "ether"));
+    await master.transfer(accounts[8], web3.utils.toWei("500"), {
+      from: accounts[1],
+    });
     var tokens = web3.utils.toWei("2", "ether");
     var tokens2 = web3.utils.toWei("530", "ether");
-    await master.transfer(accounts[1], tokens,{from:accounts[2]})
-    balance1 = await master.balanceOf(accounts[1])
-    await master.approve(accounts[6], tokens2,{from:accounts[1]})
+    await master.transfer(accounts[1], tokens, { from: accounts[2] });
+    balance1 = await master.balanceOf(accounts[1]);
+    await master.approve(accounts[6], tokens2, { from: accounts[1] });
     await helper.expectThrow(
-      master.transferFrom(accounts[1], accounts[8], tokens2,{from:accounts[6]})
+      master.transferFrom(accounts[1], accounts[8], "1" + tokens2.toString(), {
+        from: accounts[6],
+      })
     );
-    balance1b = await master.balanceOf(accounts[1])
+    balance1b = await master.balanceOf(accounts[1]);
     assert(
-      web3.utils.fromWei(balance1b) *1 == web3.utils.fromWei(balance1) * 1,
+      web3.utils.fromWei(balance1b) * 1 == web3.utils.fromWei(balance1) * 1,
       "Balance for acct 1 should not change "
     );
   });
@@ -98,7 +104,9 @@ contract("Staking Tests", function(accounts) {
   // });
   it("re-Staking without withdraw ", async function() {
     await helper.advanceTime(86400 * 10);
-    let withdrawreq = await master.requestStakingWithdraw({from:accounts[1]})
+    let withdrawreq = await master.requestStakingWithdraw({
+      from: accounts[1],
+    });
     let weSender = await web3.eth.abi.decodeParameter(
       "address",
       withdrawreq.logs[0].topics[1]
@@ -107,13 +115,15 @@ contract("Staking Tests", function(accounts) {
     await helper.advanceTime(86400 * 10);
     let s = await master.getStakerInfo(accounts[1]);
     assert(s[0] != 1, "is not Staked");
-    await master.depositStake({from:accounts[1]})
+    await master.depositStake({ from: accounts[1] });
     s = await master.getStakerInfo(accounts[1]);
     assert(s[0] == 1, "is not Staked");
   });
   it("withdraw and re-stake", async function() {
     await helper.advanceTime(86400 * 10);
-    let withdrawreq = await master.requestStakingWithdraw({from:accounts[1]})
+    let withdrawreq = await master.requestStakingWithdraw({
+      from: accounts[1],
+    });
     let weSender = await web3.eth.abi.decodeParameter(
       "address",
       withdrawreq.logs[0].topics[1]
@@ -122,10 +132,10 @@ contract("Staking Tests", function(accounts) {
     await helper.advanceTime(86400 * 10);
     let s = await master.getStakerInfo(accounts[1]);
     assert(s[0] != 1, "is not Staked");
-    await master.withdrawStake({from:accounts[1]})
+    await master.withdrawStake({ from: accounts[1] });
     s = await master.getStakerInfo(accounts[1]);
     assert(s[0] != 1, " not Staked");
-    await master.depositStake({from:accounts[1]})
+    await master.depositStake({ from: accounts[1] });
     s = await master.getStakerInfo(accounts[1]);
     assert(s[0] == 1, " Staked");
   });
