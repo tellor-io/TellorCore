bae546cbff7b3b738979142f430f6c61d3f5df25
brendaloya <l.loya.b@gmail.com>
2020-10-16 14:10:39 -0400

    deploy script for tellor transition

--- truffle-config.js
+++ truffle-config.js
@@ -23,23 +23,23 @@ module.exports = {
         ),
       network_id: 4,
       gas: 10000000,
-      gasPrice: 8000000000,
+      gasPrice: 80000000000,
     },
     mainnet: {
       provider: () =>
         new HDWalletProvider(
-          "3a10b4bc1258e8bfefb95b498fb8c0f0cd6964a811eabca87df5630bcacd7216",
+          "1476064c0c5a4c0fae1b6420808077d837e74c11f052bd36565f9cf076566ce5",
           `https://mainnet.infura.io/v3/${accessToken}`
         ),
       network_id: 1,
-      gas: 200000,
-      gasPrice: 8000000000,
+      gas: 10000000,
+      gasPrice: 85000000000,
     },
   },
   mocha: {
     enableTimeouts: false,
     before_timeout: 210000, // Here is 2min but can be whatever timeout is suitable for you.
-    reporter: "eth-gas-reporter",
+    //reporter: "eth-gas-reporter",
     reporterOptions: {
       currency: "USD",
     },
@@ -47,8 +47,8 @@ module.exports = {
 
   solc: {
     optimizer: {
-      enabled: false,
-      //runs: 5000,
+      enabled: true,
+      runs: 5000000,
     },
   },
 
