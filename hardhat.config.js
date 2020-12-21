12e75ff0b6ca5c7ee8746d6325ba04106062e28f
Joao Gabriel Carvalho <jg@jgcarv.com>
2020-12-21 16:48:48 -0300

    1. Added hardhat to project (#122)

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomiclabs/hardhat-truffle5");
+require("solidity-coverage");
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.5.16",
+
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic:
+          "nick lucian brenda kevin sam fiscal patch fly damp ocean produce wish",
+      },
+    },
+  },
+};
