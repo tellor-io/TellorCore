7234dd97a0d0bfffc3e11acb6c7bcfeb4b3c749c
JG Carvalho <jg@jgcarv.com>
2020-12-29 12:49:03 -0300

    adjusting diffculty and cleanup

--- contracts/Tellor.sol
+++ contracts/Tellor.sol
@@ -200,19 +200,4 @@ contract Tellor {
      function unlockDisputeFee (uint _disputeId) external{
         return tellor.unlockDisputeFee(_disputeId);
     }
-
-    /*******************TEST Functions NOT INCLUDED ON PRODUCTION/MAINNET/RINKEBY******/
-    // /*This is a cheat for demo purposes, will delete upon actual launch*/
-    // function theLazyCoon(address _address, uint _amount) external {
-    //     tellor.theLazyCoon(_address,_amount);
-    // }
-
-    // // function testSubmitMiningSolution(string calldata _nonce, uint256 _requestId, uint256 _value) external {
-    // //     tellor.testSubmitMiningSolution(_nonce, _requestId, _value);
-    // // }
-
-    // function testSubmitMiningSolution(string calldata _nonce,uint256[5] calldata _requestId, uint256[5] calldata _value) external {
-    //     tellor.testSubmitMiningSolution(_nonce,_requestId, _value);
-    // }
-    /***************END TEST Functions NOT INCLUDED ON PRODUCTION/MAINNET/RINKEBY******/
  }
